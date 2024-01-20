9994aeb16778cbec4f2644d8d14b4a316c262d19
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-01-20 08:56:30 -0500

    Refactor App component and import new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
-import { CORE_CONCEPTS } from './data';  // .JS required on data? lets see.
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const randomIndex = genRandomInt(reactDescriptions.length - 1);
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[randomIndex]} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
+  function handleTabSelect() {
+    console.log('Tab selected!');
+  }
+
   return (
     <div>
       <Header />
@@ -50,6 +24,16 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleTabSelect}>Components</TabButton>
+            <TabButton onSelect={handleTabSelect}>JSX</TabButton>
+            <TabButton onSelect={handleTabSelect}>Props</TabButton>
+            <TabButton onSelect={handleTabSelect}>State</TabButton>
+          </menu>
+
         </section>
       </main>
     </div>
