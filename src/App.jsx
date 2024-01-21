3855db6de973d637c05673103f3781cfe5023d10
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-01-21 11:35:53 -0500

    next lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
 
 function App() {
-  function handleTabSelect() {
-    console.log('Tab selected!');
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleTabSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please choose a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>);
   }
 
   return (
@@ -18,22 +35,18 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleTabSelect}>Components</TabButton>
-            <TabButton onSelect={handleTabSelect}>JSX</TabButton>
-            <TabButton onSelect={handleTabSelect}>Props</TabButton>
-            <TabButton onSelect={handleTabSelect}>State</TabButton>
+            <TabButton onSelect={() => handleTabSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleTabSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleTabSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleTabSelect('state')}>State</TabButton>
           </menu>
-
+          {tabContent}
         </section>
       </main>
     </div>
